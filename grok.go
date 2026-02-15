b31939eea148fe2158386e34bac529e59879fbcf
Kade710 <jonathonkanderson3@gmail.com>
2026-02-15 12:29:43 -0500

    commit

--- grok.go
+++ grok.go
@@ -1,1 +1,28 @@
-// test
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    g := grok.New()
+
+    patterns := map[string]string{
+        "NGINX_HOST":         `(?:%{IP:destination.ip}|%{NGINX_NOTSEPARATOR:destination.domain})(:%{NUMBER:destination.port:int})?`,
+        "NGINX_NOTSEPARATOR": `"[^\t ,:]+"`,
+    }
+
+    g.AddPatterns(patterns)
+    err := g.Compile("%{NGINX_HOST}", true)
+    if err != nil {
+        panic(err)
+    }
+
+    res, err := g.ParseTypedString("127.0.0.1:1234")
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(res)
+}
+
